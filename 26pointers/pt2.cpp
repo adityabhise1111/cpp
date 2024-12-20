6788a4f126fd81c69d1b357152a7350aa96b4dc2
ADITYA BHISE <136826156+adityabhise1111@users.noreply.github.com>
2024-12-20 10:00:59 +0000

    hi

--- 26pointers/pt2.cpp
+++ 26pointers/pt2.cpp
@@ -5,8 +5,19 @@ using namespace std;
 int main() {
     // Your code here
     int arr[5] = {1, 2, 3, 4, 5};
-    cout<< "address of 1st memmory bllock is "<<arr<<endl;
+    cout<< "address of 1st memmory bllock is "<<arr<<endl;//direct name represents addr of arr
+    cout<<arr[0]<<endl;
+    cout<<&arr[0]<<endl;//actually the addr of 1st element in arr is the addr of whole arr
+
     cout<< "address of 1st memory block is " << &arr[0]<<endl;
+
+    cout<<"4th"<<*arr<<endl; // * derefrence the value it carries & gives reference 
+    cout<<"5th"<<*arr +1<<endl;// value at arr plus 1[1st position +1]
+    cout<<"6th"<<*(arr+1)<<endl;// value at 2nd posion +1
+    cout<<"7th"<<*(arr)+1<<endl;//
+    cout<<"8th"<<arr[2]<<endl;  // arr[i]= *(arr+i)
+    cout<<"9th"<<*arr+2<<endl; //  i[arr]= *(i+arr)
+     
 
     return 0;
 }

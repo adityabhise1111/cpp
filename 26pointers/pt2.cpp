7b880bf7179b079b63626ca7aed7a3f1d8946196
ADITYA BHISE <136826156+adityabhise1111@users.noreply.github.com>
2024-12-21 08:37:57 +0000

    com

--- 26pointers/pt2.cpp
+++ 26pointers/pt2.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+    /*
     // Your code here
     int arr[5] = {1, 2, 3, 4, 5};
     cout<< "address of 1st memmory bllock is "<<arr<<endl;//direct name represents addr of arr
@@ -17,7 +18,20 @@ int main() {
     cout<<"7th"<<*(arr)+1<<endl;//
     cout<<"8th"<<arr[2]<<endl;  // arr[i]= *(arr+i)
     cout<<"9th"<<*arr+2<<endl; //  i[arr]= *(i+arr)
-     
+    
 
+   int temp[10];
+   cout << sizeof(temp)<<endl;
+   
+    cout << (&temp)<<endl;
+    cout << sizeof(&temp)<<endl;
+    cout << sizeof(*temp)<<endl;
+    int *ptr = &temp[0];
+    cout << sizeof(ptr)<<endl; 
+    cout << sizeof(*ptr)<<endl;
+    cout << sizeof(&ptr)<<endl;
+    */
+    
+    cout << <<endl;
     return 0;
 }

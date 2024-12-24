a1fe5439d638ca316ceda42c62909786d74e1f4e
ADITYA BHISE <136826156+adityabhise1111@users.noreply.github.com>
2024-12-24 08:14:41 +0000

    jaered

--- 28ref/dym3.cpp
+++ 28ref/dym3.cpp
@@ -3,31 +3,54 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    int m,n;
-    cin >> m >> n;  
-    //lets create a d array
-    int **arr = new int*[m];
-    for ( int i =0 ; i<m; i++){
-        arr[i]= new int[n];
+    // cout << "Hello, World!" << endl;
+    // int m,n;
+    // cin >> m >> n;  
+    // //lets create a d array
+    // int **arr = new int*[m];
+    // for ( int i =0 ; i<m; i++){
+    //     arr[i]= new int[n];
+    // }
+    // //insert in to arr
+    // for (int i =0; i<m; i++)
+    // {
+    //     for (int j=0; j< n; j++){
+    //         cin>>arr[i][j];
+    //     }
+    // }
+    // //print the arrray 
+    //  for ( int i=0; i<m; i++) {
+    //     for ( int j=0 ; j<n; j++){
+    //         cout<< arr[i][j]<<" "; 
+    //     }
+    //     cout<<endl;
+    //  }
+    //  //dont forget to delete the memory
+    //  for (int i=0; i<m ; i++){
+    //     delete [] arr[i];
+    //  }
+
+    //jaggered array
+    int row = 3;
+    int col[]={5,7,9};
+
+    int **arr = new int*[row];
+    for (int i=0; i<row;i++){
+        arr[i]= new int[col[i]];
+
     }
-    //insert in to arr
-    for (int i =0; i<m; i++)
-    {
-        for (int j=0; j< n; j++){
+    for(int i=0; i<row; i++){
+        for (int j=0; j<col[i]; j++){
             cin>>arr[i][j];
         }
     }
-    //print the arrray 
-     for ( int i=0; i<m; i++) {
-        for ( int j=0 ; j<n; j++){
-            cout<< arr[i][j]<<" "; 
+    for (int i=0; i<row; i++){
+        for (int j=0; j<col[i]; j++){
+            cout<<arr[i][j]<<" ";
         }
         cout<<endl;
-     }
-     //dont forget to delete the memory
-     for (int i=0; i<m ; i++){
-        delete [] arr[i];
-     }
+    }
+
+
     return 0;
 }

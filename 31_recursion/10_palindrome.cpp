dcfc915696fed272e5652978228ab089b4e2ff30
ADITYA BHISE <136826156+adityabhise1111@users.noreply.github.com>
2024-12-28 09:00:09 +0000

    palindrome complete

--- 31_recursion/10_palindrome.cpp
+++ 31_recursion/10_palindrome.cpp
@@ -2,13 +2,19 @@
 #include<string>
 using namespace std;
 
-is_palindrome(string){
+bool is_palindrome(string str,int i=0, int j=-1){
+    if(j==-1) {
+        j=str.size() -1;
+    }
+    if(str[i]!=str[j]) return false;
+    if(i>=j) return true;
+    return (is_palindrome(str,i+1,j-1));
     
 }
 
 int main(){
     string name;
-    cin>> name>>endl;
+    cin>> name;
     cout<<is_palindrome(name)<<endl;
 
 }
